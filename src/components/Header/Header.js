3a9efa6263a940dd9ea11adad19f53e8d15a8575
mohit <mohitkumar2001311@gmail.com>
2023-12-31 21:43:22 +0530

    second commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,60 +3,6 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      {/* <div className="row" style={{ backgroundColor: "#7EC029" }}>
-        <div className="container row d-flex justify-content-between">
-          <div><img src="/logo_zenith.png" alt="" /></div>
-
-          <div>
-            <button type="button" class="btn btn-danger">
-              Registration
-            </button>
-          </div>
-
-          <div >
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              className="text-decoration-none me-3"
-            >
-              <i
-                className="fab fa-facebook fa-2x"
-                style={{ color: "#165216" }}
-              />
-            </a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              className="text-decoration-none me-3"
-            >
-              <i
-                className="fab fa-twitter fa-2x"
-                style={{ color: "#165216" }}
-              />
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              className="text-decoration-none me-3"
-            >
-              <i
-                className="fab fa-instagram fa-2x"
-                style={{ color: "#165216" }}
-              />
-            </a>
-            <a
-              href="https://wa.me/1234567890"
-              target="_blank"
-              className="text-decoration-none me-3"
-            >
-              <i
-                className="fab fa-whatsapp fa-2x"
-                style={{ color: "#165216" }}
-              />
-            </a>
-          </div>
-        </div>
-      </div> */}
       <div
         className="d-flex justify-content-between "
         style={{ backgroundColor: "#7EC029", height: "60px" }}
@@ -79,9 +25,9 @@ const Header = () => {
           </button>
             </div>
             <div>
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
+            <a 
+              href="https://www.facebook.com/ZenithAcademyPvtLtd/"
+              target=""
               className="text-decoration-none me-3"
             >
               <i
@@ -92,8 +38,8 @@ const Header = () => {
             </div>
           <div>
             <a
-              href="https://www.youtube.com/"
-              target="_blank"
+              href="https://www.youtube.com/@zenithacademysince95"
+              target=""
               className="text-decoration-none me-3"
             >
               <i
@@ -104,8 +50,8 @@ const Header = () => {
           </div>
           <div style={{marginRight:"1rem"}}>
             <a
-              href="https://www.instagram.com/"
-              target="_blank"
+              href="https://www.instagram.com/zenithacademysince95/"
+              target=""
               className="text-decoration-none me-3"
             >
               <i
